844bd7e1e6f6c3aafca8b51a8b3e774f482867c4
Orestis Tsirakis <orestistsira@gmail.com>
2022-12-21 15:06:43 +0200

    Update README.md, asyncKnn.c, and knn.h

---
+++ asyncKnn.c
@@ -0,0 +1,26 @@
+#include "knn.h"
+
+knnresult distrAllkNN(double* X, int n, int d, int k){
+    int SelfTID, NumTasks, t, data;
+    MPI_Status mpistat;
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size( MPI_COMM_WORLD, &NumTasks );
+    MPI_Comm_rank( MPI_COMM_WORLD, &SelfTID );
+    printf("Hello World from %i of %i\n", SelfTID,NumTasks);
+
+    if( SelfTID == 0 ) {
+        for(t=1;t<NumTasks;t++) {
+            data = t;
+            MPI_Send(&data,1,MPI_INT,t,55,MPI_COMM_WORLD);
+        }
+    } else {
+        MPI_Recv(&data,1,MPI_INT,0,55,MPI_COMM_WORLD,&mpistat);
+        printf("TID%i: received data=%i\n",SelfTID,data);
+    }
+
+    MPI_Finalize();
+
+    knnresult knn;
+    return knn;
+}
--- knn.h
+++ knn.h
@@ -5,6 +5,7 @@
 #include <sys/time.h>
 #include <cblas.h>
 #include <omp.h>
+#include <mpi.h>
 
 typedef struct knnresult{
     int* nidx;
@@ -16,3 +17,5 @@ typedef struct knnresult{
 void printArrayDouble(double* arr, int size);
 
 knnresult kNN(double* X, double* Y, int n, int m, int d, int k);
+
+knnresult distrAllkNN(double* X, int n, int d, int k);
